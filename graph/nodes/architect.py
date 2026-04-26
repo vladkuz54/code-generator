fc0fd149746aae10e2bd7aeff34b0688a9a0a99f
vladkuz54 <vkuz1540@gmail.com>
2026-04-26 20:19:19 +0300

    Fix import statement and handle missing tester_bug_report gracefully

--- graph/nodes/architect.py
+++ graph/nodes/architect.py
@@ -1,14 +1,13 @@
 from typing import Any, Dict
 
-from chains import architect_chain
-
+from graph.chains.architect_chain import architect_chain
 from graph.state import GraphState
 
 
 def architect(state: GraphState) -> Dict[str, Any]:
     print("---ARCHITECT---")
     query = state["query"]
-    tester_bug_report = state["tester_bug_report"]
+    tester_bug_report = state.get("tester_bug_report", None)
 
     architect_task = architect_chain.invoke(
         {"query": query, "tester_bug_report": tester_bug_report}
