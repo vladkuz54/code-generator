b7279776d3a54277b3563eb5bd04e4e3f892cff6
vladkuz54 <vkuz1540@gmail.com>
2026-04-26 14:26:26 +0300

    Enhance architect function to include tester bug report in task invocation

--- graph/nodes/architect.py
+++ graph/nodes/architect.py
@@ -8,7 +8,10 @@ from graph.state import GraphState
 def architect(state: GraphState) -> Dict[str, Any]:
     print("---ARCHITECT---")
     query = state["query"]
+    tester_bug_report = state["tester_bug_report"]
 
-    architect_task = architect_chain.invoke({"query": query})
+    architect_task = architect_chain.invoke(
+        {"query": query, "tester_bug_report": tester_bug_report}
+    )
 
     return {"architect_task": architect_task}
